cebd5e1cb5ea47e50c4baf28eeb5858ca077cbcc
Hiltonboth <hitol.homes@gmail.com>
2024-02-13 09:41:26 +0200

    webfolder

--- apps/web/src/components/(dashboard)/period-selector/types.ts
+++
@@ -1,6 +0,0 @@
-export type PeriodSelectorValue = '' | '7d' | '14d' | '30d';
-
-export const isPeriodSelectorValue = (value: unknown): value is PeriodSelectorValue => {
-  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-  return ['', '7d', '14d', '30d'].includes(value as string);
-};
